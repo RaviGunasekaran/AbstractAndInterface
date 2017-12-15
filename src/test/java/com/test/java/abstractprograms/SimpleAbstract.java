90fd500d038a8f064b228b1e0e76f843141ce634
Ravi Gunasekaran <rgunasekaran@altimetrik.com>
2017-12-15 15:27:57 +0530

    Initial commit for AbstractAndInterface programs

---
+++ src/test/java/com/test/java/abstractprograms/SimpleAbstract.java
@@ -0,0 +1,10 @@
+package com.test.java.abstractprograms;
+
+public abstract class SimpleAbstract {
+
+	public static void main(String[] args) {
+		System.out.println("This is Simple Abstract Class");
+
+	}
+
+}
